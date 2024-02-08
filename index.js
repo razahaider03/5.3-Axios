189793d503f155e2103b3e9e58fc17df863f2c8a
razahaider03 <54476563+razahaider03@users.noreply.github.com>
2024-02-08 14:28:55 +0000

    complete project

--- index.js
+++ index.js
@@ -30,6 +30,19 @@ app.post("/", async (req, res) => {
   // Step 2: Play around with the drop downs and see what gets logged.
   // Use axios to make an API request to the /filter endpoint. Making
   // sure you're passing both the type and participants queries.
+  try {
+    const response = await axios.get(`https://bored-api.appbrewery.com/filter?type=${req.body.type}&participants=${req.body.participants}`);
+    const result = response.data;
+    const randomIndex = Math.floor(Math.random() * result.length);
+    console.log(result[randomIndex]);
+    // console.log(result);
+    res.render("index.ejs", { data: result[randomIndex] });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
   // Render the index.ejs file with a single *random* activity that comes back
   // from the API request.
   // Step 3: If you get a 404 error (resource not found) from the API request.
